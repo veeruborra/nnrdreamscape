f33714cb5c17adc2e0116865c0aed15d45162bd5
raghumaturu <19.raghu.m@gmail.com>
2020-02-13 18:44:07 +0530

    landing

--- src/app/cases/cases.component.ts
+++ src/app/cases/cases.component.ts
@@ -21,5 +21,59 @@ export class CasesComponent implements OnInit {
        }
     );
   }
+  over(elem: HTMLElement) {
+    let elem1: HTMLElement = document.getElementById("contentDiv0");
+    let elem2: HTMLElement = document.getElementById("contentDiv1");
+    let elem3: HTMLElement = document.getElementById("contentDiv2");
+    let img1: HTMLElement = document.getElementById("images0");
+    let img2: HTMLElement = document.getElementById("images1");
+    let img3: HTMLElement = document.getElementById("images2");
+    if (elem.classList.contains("contentDiv0")){
+      elem1.style.width = "70%";
+      elem2.style.width = "15%";
+      elem3.style.width = "15%";
 
+        // img1.style.width = "100%";
+        // img2.style.width = "33.333vw";
+        // img3.style.width = "33.333vw";
+
+    } else if (elem.classList.contains("contentDiv1")) {
+      elem1.style.width = "15%";
+      elem2.style.width = "70%";
+      elem3.style.width = "15%";
+
+      // img1.style.width = "33.333vw";
+      // img2.style.width = "100%";
+      // img3.style.width = "33.333vw";
+
+    } else if (elem.classList.contains("contentDiv2")) {
+      elem1.style.width = "15%";
+      elem2.style.width = "15%";
+      elem3.style.width = "70%";
+
+      // img1.style.width = "33.333vw";
+      // img2.style.width = "33.333vw";
+      // img3.style.width = "100%";
+    } 
+    elem.classList.remove('rotateText');
+  }
+
+  out(elem: HTMLElement) {
+    let elem1: HTMLElement = document.getElementById("contentDiv0");
+    let elem2: HTMLElement = document.getElementById("contentDiv1");
+    let elem3: HTMLElement = document.getElementById("contentDiv2");
+
+    let img1: HTMLElement = document.getElementById("images0");
+    let img2: HTMLElement = document.getElementById("images1");
+    let img3: HTMLElement = document.getElementById("images2");
+    
+    // img1.style.width = "33.333vw";
+    // img2.style.width = "33.333vw";
+    // img3.style.width = "33.333vw";
+
+    elem1.style.width = "33%";
+    elem2.style.width = "33%";
+    elem3.style.width = "33%";
+    elem.classList.add('rotateText');
+  }
 }
